141ef2365a96685c8d7f39064c9d44815fa7716f
Francisco del Collado <fedelcollado@gmail.com>
2024-01-22 14:22:48 -0600

    password recovery

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -41,4 +41,12 @@ export class AuthService {
       email
     });
   }
+
+  recovery(email: string) {
+    return this.http.post(`${this.apiUrl}/api/v1/auth/recovery`, {email})
+  }
+
+  changePassword(token: string, newPassword: string) {
+    return this.http.post(`${this.apiUrl}/api/v1/auth/change-password`, {token, newPassword});
+  }
 }
