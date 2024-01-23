b88af55c999ebd8bafdd31b93405c385ce5d62d0
Francisco del Collado <fedelcollado@gmail.com>
2024-01-22 18:05:10 -0600

    redirect para recuperar la sesion

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { authGuard } from '@guards/auth.guard';
+import { redirectGuard } from '@guards/redirect.guard';
 
 const routes: Routes = [
   {
     path: '',
+    canActivate:[redirectGuard],
     loadChildren: () => import('./modules/auth/auth.module').then((m) => m.AuthModule),
   },
   {
---
+++ src/app/guards/redirect.guard.ts
@@ -0,0 +1,14 @@
+import { inject} from '@angular/core';
+import { CanActivateFn, Router} from '@angular/router';
+import { TokenService } from '@services/token.service';
+
+export const redirectGuard: CanActivateFn = () => {
+  const token = inject(TokenService).getToken();
+  const router = inject(Router);
+
+  if (token) {
+    router.navigate(['/app']);
+  }
+  return true;
+}
+
