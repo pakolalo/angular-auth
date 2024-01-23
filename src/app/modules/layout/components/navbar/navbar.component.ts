1ef7a2df552c1e02bb9887e6586c5db727f6351f
Francisco del Collado <fedelcollado@gmail.com>
2024-01-23 14:11:02 -0600

    obtener el perfil

--- src/app/modules/layout/components/navbar/navbar.component.ts
+++ src/app/modules/layout/components/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import {
   faBell,
@@ -6,13 +6,14 @@ import {
   faClose,
   faAngleDown
 } from '@fortawesome/free-solid-svg-icons';
+import { User } from '@models/user.model';
 import { AuthService } from '@services/auth.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
   faBell = faBell;
   faInfoCircle = faInfoCircle;
   faClose = faClose;
@@ -21,10 +22,19 @@ export class NavbarComponent {
   isOpenOverlayAvatar = false;
   isOpenOverlayBoards = false;
 
+  user: User | null = null;
+
   constructor(
     private authService: AuthService,
     private router: Router,
   ) {}
+
+  ngOnInit() {
+    this.authService.getProfile()
+    .subscribe(user => {
+      this.user = user;
+    })
+  }
 
   logout() {
     this.authService.logout();
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -4,6 +4,7 @@ import { environment } from '@environments/environment';
 import { switchMap, tap } from 'rxjs';
 import { TokenService } from './token.service';
 import { ResponseLogin} from '@models/auth.model';
+import { User } from '@models/user.model';
 
 
 @Injectable({
@@ -57,6 +58,15 @@ export class AuthService {
 
   changePassword(token: string, newPassword: string) {
     return this.http.post(`${this.apiUrl}/api/v1/auth/change-password`, {token, newPassword});
+  }
+
+  getProfile() {
+    const token = this.tokenService.getToken()
+    return this.http.get<User>(`${this.apiUrl}/api/v1/auth/profile`, {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    })
   }
 
   logout() {
